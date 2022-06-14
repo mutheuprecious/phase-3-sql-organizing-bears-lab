b362e401aca47f82d033e6cee10f678c4be6379e
Precious <preciousmaregwa1997@gmail.com>
2022-06-14 21:04:59 +0300

    commit tree 7ecc87b555990bc3189164d5baa37182720b449a

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 25, "M", "Gray", "quiet", 0),
+("Rowdy", 8, "F", "White", "noisy", 1),
+("Tabitha", 12, "M", "Brown", "calm", 1),
+("Sergeant Brown", 32, "F", "Gray", "aggressive", 0),
+("Melissa", 16, "F", "Dark Gray", "friendly", 1),
+("Grinch", 48, "M", "Pink", "sweet", 0),
+("Wendy", 23, "F", "Gray", "choleric", 1),
+(NULL, 27, "M", "Blue", "calm", 0);
